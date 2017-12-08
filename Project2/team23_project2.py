45317095db9206bcb9e095e0c79cb9a02273eb8b
Cullen Sturdivant <potatophuison@gmail.com>
2017-12-08 02:45:19 -0600

    Skeleton of simulator output complete

--- Project2/team23_project2.py
+++ Project2/team23_project2.py
@@ -1,6 +1,18 @@
 from sys import argv
 import math
 
+#========================================
+# Command Line Arguments
+#========================================
+for i in range(len(argv)):
+    if (argv[i] == '-i' and i < (len(argv) - 1)):  # make sure there are at least 2 args
+        inputFileName = argv[i + 1]		    
+    elif (argv[i] == '-o' and i < (len(argv) - 1)):
+        outputFileName = argv[i + 1]
+
+inFile = open(inputFileName, "r")
+disFile = open(outputFileName + "_dis.txt", "w")
+pipelineFile = open(outputFileName + "_pipeline.txt", "w")
 
 #========================================
 # Disassembler Class
@@ -14,9 +26,9 @@ class Disassembler:
     mem = []            # values stored in memory after the BREAK instruction
     valids = []         # indecies of valid instructions
     addrs = []          # addresses of the instructions in their respective order
-    arg1 = []           # list of all first args in order of instructions (-1 = N/A)
-    arg2 = []           # list of all second args in order of instructions (-1 = N/A)
-    arg3 = []           # list of all third args in order of instructions (-1 = N/A)
+    args1 = []          # list of all first args in order of instructions (-1 = N/A)
+    args2 = []          # list of all second args in order of instructions (-1 = N/A)
+    args3 = []          # list of all third args in order of instructions (-1 = N/A)
                         # arg3 INCLUDES IMMEDIATE VALUES
     instStrings = []    # the formatted string of the instruction
 
@@ -32,20 +44,19 @@ class Disassembler:
     #############
     # Evaluates the 2's Complement form of a binary number
     def twosComp(self, binString):
-        return (int(binString[1:], 2) - ((2 * int(binString[0], 2)) ** (len(binString) - 1)))
+        return (int(binString[1:], 2) - ((int(binString[0], 2)) << (len(binString) - 1)))
 
     #############
     # Disassembles the list of binary strings
     def disassemble(self):
         breakFound = False
+        PC = 96
 
         for i in range(len(self.binaryList)):
             if (breakFound == False):
-                self.numInstrs += 1
-                self.instructions.append(self.binaryList[i])
-
-                breakFound = self.determineInstruction(self.binaryList[i])
+                breakFound = self.determineInstruction(self.binaryList[i], PC)
                 self.instStrings.append(self.toString(self.binaryList[i]))
+                PC += 4
             else:
                 self.mem.append(self.twosComp(self.binaryList[i]))
     
@@ -53,19 +64,22 @@ class Disassembler:
     # Breaks up the instruction and stores it in the appropriate list
     #
     # Returns TRUE if break instruction was found
-    def determineInstruction(self, inst):
+    def determineInstruction(self, inst, PC):
 
         if (int(inst[:1], 2) == 0):
             self.valids.append(0)
             return False
 
+        self.addrs.append(PC)
+        self.instructions.append(inst)
+        self.numInstrs += 1
         opcode = int(inst[1:6], 2)
         self.opcodes.append(opcode)
         
         if (opcode == 0 or opcode == 28):
-            self.arg1.append(int(inst[6:11], 2))
-            self.arg2.append(int(inst[11:16], 2))
-            self.arg3.append(int(inst[16:21], 2))
+            self.args1.append(int(inst[6:11], 2))
+            self.args2.append(int(inst[11:16], 2))
+            self.args3.append(int(inst[16:21], 2))
 
             functionCode = int(inst[-6:], 2)
 
@@ -74,14 +88,14 @@ class Disassembler:
                 return True
         
         elif (opcode == 2):
-            self.arg1.append(-(int(inst[6:11], 2)))
-            self.arg2.append(-(int(inst[11:16], 2)))
-            self.arg3.append(-(int(inst[16:21], 2)))
+            self.args1.append(-(int(inst[6:11], 2)))
+            self.args2.append(-(int(inst[11:16], 2)))
+            self.args3.append(-(int(inst[16:21], 2)))
 
         else:
-            self.arg1.append(int(inst[6:11], 2))
-            self.arg2.append(int(inst[11:16], 2))
-            self.arg3.append(int(inst[16:], 2))
+            self.args1.append(int(inst[6:11], 2))
+            self.args2.append(int(inst[11:16], 2))
+            self.args3.append(self.twosComp(inst[16:]))
 
         return False
 
@@ -202,6 +216,189 @@ class Disassembler:
             index += 1
 
         
+#========================================
+# Simulator Class
+#========================================
+class Simulator:
+
+    class Fetch:
+        def __init__(self):
+            print "Init Fetch"
+        def run(self):
+            print "Run Fetch"
+            return False
+
+    class Issue:
+        def run(self):
+            print "Run Issue"
+
+    class LogicUnit:
+        def run(self):
+            print "Run ALU"
+
+    class MemoryUnit:
+        def run(self):
+            print "Run MemoryUnit"
+
+    class WriteBack:
+        def run(self):
+            print "Run WriteBack Unit"
+
+
+    fetch = Fetch()
+    issue = Issue()
+    ALU = LogicUnit()
+    MEM = MemoryUnit()
+    WB = WriteBack()
+
+    registers = [0] * 32
+    instructions = []
+    opcodes = []
+    memory = []
+    valids = []
+    addresses = []
+    numInstructions = []
+    args1 = []
+    args2 = []
+    args3 = []
+
+    numInstructions = 0
+    cycle = 1
+
+
+    #****************************************************
+    # Constructor for the Simulator Class
+    #****************************************************
+    def __init__(self, insts, opcodes, mem, valids, addrs, args1, args2, args3, numInstrs):
+        self.instructions = insts
+        self.opcodes = opcodes
+        self.memory = mem
+        self.valids = valids
+        self.addresses = addrs
+        self.numInstructions = numInstrs
+        self.args1 = args1
+        self.args2 = args2
+        self.args3 = args3
+
+    #****************************************************
+    # Prints all information about the state of the
+    # entire machine at the moment the function is called
+    #****************************************************
+    def printState(self):
+        pipelineFile.write("--------------------\n")
+        pipelineFile.write("Cycle:" + str(self.cycle) + '\n')
+
+        pipelineFile.write("\nPre-Issue Buffer:\n")
+        pipelineFile.write("\tEntry 0:\t")
+        # Buffer 0 contents go here
+        pipelineFile.write('\n')
+        pipelineFile.write("\tEntry 1:\t")
+        # Buffer 1 contents go here
+        pipelineFile.write('\n')
+        pipelineFile.write("\tEntry 2:\t")
+        # Buffer 2 contents go here
+        pipelineFile.write('\n')
+        pipelineFile.write("\tEntry 3:\t")
+        # Buffer 3 contents go here
+        pipelineFile.write('\n')
+
+        pipelineFile.write("Pre_ALU Queue:\n")
+        pipelineFile.write("\tEntry 0:\t")
+        # Entry 0 goes here
+        pipelineFile.write('\n')
+        pipelineFile.write("\tEntry 1:\t")
+        # Entry 1 goes here
+        pipelineFile.write('\n')
+
+        pipelineFile.write("Post_ALU Queue:\n")
+        pipelineFile.write("\tEntry 0:\t")
+        # Entry 0 contents go here
+        pipelineFile.write('\n')
+
+        pipelineFile.write("Pre_MEM Queue:\n")
+        pipelineFile.write("\tEntry 0:\t")
+        # Entry 0 contents go here
+        pipelineFile.write('\n')
+        pipelineFile.write("\tEntry 1:\t")
+        # Entry 1 contents go here
+        pipelineFile.write('\n')
+
+        pipelineFile.write("Post_MEM Queue:\n")
+        pipelineFile.write("\tEntry 0:\t")
+        # Entry 0 contents go here
+        pipelineFile.write("\n\n")
+
+        ##### Register output #####
+        pipelineFile.write("Registers\n")
+        pipelineFile.write("R00:\t")
+        for i in range(8):
+            pipelineFile.write(str(self.registers[i]) + "\t")
+        pipelineFile.write("\n")
+
+        pipelineFile.write("R08:\t")
+        for i in range(8, 16):
+            pipelineFile.write(str(self.registers[i]) + "\t")
+        pipelineFile.write("\n")
+
+        pipelineFile.write("R16:\t")
+        for i in range(16, 24):
+            pipelineFile.write(str(self.registers[i]) + "\t")
+        pipelineFile.write("\n")
+
+        pipelineFile.write("R24:\t")
+        for i in range(24, 32):
+            pipelineFile.write(str(self.registers[i]) + "\t")
+        pipelineFile.write("\n\n")
+
+        ##### Cache Output #####
+        pipelineFile.write("Cache\n")
+        pipelineFile.write("Set 0: LRU=")
+        # LRU value goes here
+        pipelineFile.write("\n\tEntry 0:")
+        # Cache info goes here
+        pipelineFile.write("\n\tEntry 1:")
+        # Cache info goes here
+        pipelineFile.write("\nSet 1: LRU=")
+        # LRU value goes here
+        pipelineFile.write("\n\tEntry 0:")
+        # Cache info goes here
+        pipelineFile.write("\n\tEntry 1:")
+        # Cache info goes here
+        pipelineFile.write("\nSet 2: LRU=")
+        # LRU value goes here
+        pipelineFile.write("\n\tEntry 0:")
+        # Cache info goes here
+        pipelineFile.write("\n\tEntry 1:")
+        # Cache info goes here
+        pipelineFile.write("\nSet 3: LRU=")
+        # LRU value goes here
+        pipelineFile.write("\n\tEntry 0:")
+        # Cache info goes here
+        pipelineFile.write("\n\tEntry 1:")
+        # Cache info goes here
+        pipelineFile.write("\n\n")
+
+        ##### Data Output #####
+        pipelineFile.write("Data")
+        for i in range(len(self.memory)):
+            if (i % 8 == 0):
+                pipelineFile.write('\n')
+                pipelineFile.write(str(self.addresses[len(self.addresses) - 1]) + ":\t")
+            pipelineFile.write(str(self.memory[i]) + '\t')
+        pipelineFile.write('\n')
+
+    def run(self):
+        go = True
+        while go:
+            self.WB.run()
+            self.ALU.run()
+            self.MEM.run()
+            self.issue.run()
+            go = self.fetch.run()
+            self.printState()
+            self.cycle += 1
+    
+
 
 #========================================
 # Variables
@@ -236,15 +433,7 @@ def disOutput(disFile, binary, PC, instString, doSpacing):
 #========================================
 # Main Code
 #========================================
-for i in range(len(argv)):
-    if (argv[i] == '-i' and i < (len(argv) - 1)):  # make sure there are at least 2 args
-        inputFileName = argv[i + 1]		    
-    elif (argv[i] == '-o' and i < (len(argv) - 1)):
-        outputFileName = argv[i + 1]
 
-inFile = open(inputFileName, "r")
-disFile = open(outputFileName + "_dis.txt", "w")
-#pipelineFile = open(outputFileName + "_pipeline.txt", "w")
 
 instructions = inFile.readlines()
 
@@ -252,8 +441,14 @@ dis = Disassembler(instructions)
 dis.disassemble()
 dis.output(disFile)
 
+sim = Simulator(dis.instructions, dis.opcodes, dis.mem, dis.valids,
+                dis.addrs, dis.args1, dis.args2, dis.args3, dis.numInstrs)
+
+sim.run()
+
 inFile.close()
 disFile.close()
+pipelineFile.close()
 
 
 
